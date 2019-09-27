e8f262ce57898c16fcc204e3178879bc021d5be9
Christopher Rusli <chrisrusli18@gmail.com>
2019-09-27 10:27:48 +0700

    Initial Commit

---
+++ Organized/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Organized
+//
+//  Created by Christopher Rusli  on 9/27/19.
+//  Copyright © 2019 Christopher Rusli . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
